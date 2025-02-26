d9fd672d4acc28783f7650ffa2ee9976823167db
khushwant <khushawantn1304@gmail.com>
2025-02-27 02:18:15 +0530

    refactor: improve formatting and add connection log in MongoDB config

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 
-const connectDB = async ()=>{
+const connectDB = async () => {
 
-    mongoose.connection.on('connected',()=>console.log("Database connected"))
+    mongoose.connection.on('connected', () => console.log("Database connected"))
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+    console.log(
+        "papa ne sikhaya hai"
+    )
 };
 
 export default connectDB
