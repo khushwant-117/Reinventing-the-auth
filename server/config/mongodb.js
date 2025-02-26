94a402b4e63098d93c66829cfafce7c882123d34
khushwant <khushawantn1304@gmail.com>
2025-02-27 02:20:25 +0530

    logging

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database connected"))
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-    console.log(
-        "papa ne sikhaya hai"
-    )
+    // console.log(
+    //     "papa ne sikhaya hai"
+    // )
 };
 
 export default connectDB
